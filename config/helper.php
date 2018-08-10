d43d78d42740db832df72d24ce9c17ce265c1f2f
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-10 15:07:11 +0800

    Added config file

---
+++ config/helper.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+	'sequence_length' => env('HELPER_SEQUENCE_LENGTH', 6),
+	'abbrv' => [
+		'remove_vowels' => ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', ' '],
+		'remove_non_alphanumeric' => true,
+		'to_uppercase' => true,
+		'unique_characters' => true,
+	]
+];
