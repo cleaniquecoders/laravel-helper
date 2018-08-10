5e907676a3a03ac1a7a3c4afa55a122d71b1ce4f
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-10 15:08:16 +0800

    Update test

---
+++ tests/HelperTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CleaniqueCoders\LaravelHelper\Tests;
+
+class HelperTest extends TestCase
+{
+    /** @test */
+    public function it_has_helper_file()
+    {
+        $this->assertTrue(file_exists(dirname(dirname(__FILE__)) . '/src/Support/helpers.php'));
+    }
+
+    /** @test */
+    public function it_has_helper_config_file()
+    {
+        $this->assertTrue(file_exists(dirname(dirname(__FILE__)) . '/config/helper.php'));
+    }
+
+    /** @test */
+    public function it_has_all_helpers()
+    {
+        $helpers = ['abbrv', 'generate_sequence', 'fqcn', 'str_slug_fqcn'];
+
+        foreach ($helpers as $helper) {
+            $this->assertTrue(function_exists($helper));
+        }
+    }
+}
